58776efd0f87a1fa0bd946665ac99873df272f50
Dhipa Poltak <dhipa2poltak@gmail.com>
2023-04-29 14:12:29 +0700

    adding splash screen

--- app/src/main/java/com/dpfht/tmdbcleanmvvm/MainActivity.kt
+++ app/src/main/java/com/dpfht/tmdbcleanmvvm/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.dpfht.tmdbcleanmvvm
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
@@ -27,5 +31,26 @@ class MainActivity : AppCompatActivity() {
 
   override fun onSupportNavigateUp(): Boolean {
     return navController.navigateUp() || super.onSupportNavigateUp()
+  }
+
+  override fun onResume() {
+    super.onResume()
+
+    registerReceiver(receiverToGenre, IntentFilter("enter_genre"))
+  }
+
+  override fun onPause() {
+    super.onPause()
+
+    unregisterReceiver(receiverToGenre)
+  }
+
+  private val receiverToGenre = object : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+      val navGraph = navController.navInflater.inflate(R.navigation.nav_graph)
+      navGraph.setStartDestination(R.id.genreFragment)
+
+      navController.graph = navGraph
+    }
   }
 }
--- app/src/main/java/com/dpfht/tmdbcleanmvvm/feature/genre/GenreFragment.kt
+++ app/src/main/java/com/dpfht/tmdbcleanmvvm/feature/genre/GenreFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavDeepLinkRequest
@@ -50,6 +51,7 @@ class GenreFragment: Fragment() {
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
+    setToolbar()
 
     adapter.genres = viewModel.genres
 
@@ -95,6 +97,10 @@ class GenreFragment: Fragment() {
     }
 
     viewModel.start()
+  }
+
+  private fun setToolbar() {
+    (requireActivity() as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(false)
   }
 
   private fun showErrorMessage(message: String) {
