7655fd220e9bab3558ba8b708a809588dc0e8cf5
Dhipa Poltak <dhipa2poltak@gmail.com>
2024-02-07 09:44:32 +0700

    implementing unit test for RestService (Retrofit) class

---
+++ framework/src/test/java/com/dpfht/tmdbcleanmvvm/framework/data/datasource/remote/rest/RestServiceTest.kt
@@ -0,0 +1,129 @@
+package com.dpfht.tmdbcleanmvvm.framework.data.datasource.remote.rest
+
+import com.dpfht.tmdbcleanmvvm.data.Constants
+import com.google.gson.Gson
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import okhttp3.OkHttpClient
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@RunWith(JUnit4::class)
+@OptIn(ExperimentalCoroutinesApi::class)
+class RestServiceTest {
+
+    private lateinit var mockWebServer: MockWebServer
+    private lateinit var restService: RestService
+    private lateinit var gson: Gson
+
+    @Before
+    fun setup() {
+        gson = Gson()
+        mockWebServer = MockWebServer()
+
+        val okHttpClient = OkHttpClient()
+            .newBuilder()
+            .addInterceptor(AuthInterceptor()).build()
+
+        restService = Retrofit.Builder()
+            .baseUrl(mockWebServer.url("/"))
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .client(okHttpClient)
+            .build().create(RestService::class.java)
+    }
+
+    @Test
+    fun `ensure the correct path is called when calling getMovieGenre method in RestService`() = runTest {
+        val mockResponse = MockResponse()
+        mockWebServer.enqueue(mockResponse.setBody("{}"))
+
+        restService.getMovieGenre()
+        val request = mockWebServer.takeRequest()
+
+        val path = request.path ?: ""
+        assertTrue(path.isNotEmpty())
+
+        assertEquals("/3/genre/movie/list", path.substring(0, path.indexOf("?")))
+        assertTrue(path.contains(Constants.API_KEY))
+    }
+
+    @Test
+    fun `ensure the correct path is called when calling getMoviesByGenre method in RestService`() = runTest {
+        val mockResponse = MockResponse()
+        mockWebServer.enqueue(mockResponse.setBody("{}"))
+
+        val genreId = 10
+        val page = 1
+        restService.getMoviesByGenre(genreId.toString(), page)
+        val request = mockWebServer.takeRequest()
+
+        val path = request.path ?: ""
+        assertTrue(path.isNotEmpty())
+
+        assertEquals("/3/discover/movie", path.substring(0, path.indexOf("?")))
+        assertTrue(path.contains(Constants.API_KEY))
+    }
+
+    @Test
+    fun `ensure the correct path is called when calling getMovieDetail method in RestService`() = runTest {
+        val mockResponse = MockResponse()
+        mockWebServer.enqueue(mockResponse.setBody("{}"))
+
+        val movieId = 101
+        restService.getMovieDetail(movieId)
+        val request = mockWebServer.takeRequest()
+
+        val path = request.path ?: ""
+        assertTrue(path.isNotEmpty())
+
+        assertEquals("/3/movie/${movieId}", path.substring(0, path.indexOf("?")))
+        assertTrue(path.contains(Constants.API_KEY))
+    }
+
+    @Test
+    fun `ensure the correct path is called when calling getMovieReviews method in RestService`() = runTest {
+        val mockResponse = MockResponse()
+        mockWebServer.enqueue(mockResponse.setBody("{}"))
+
+        val movieId = 101
+        val page = 1
+        restService.getMovieReviews(movieId, page)
+        val request = mockWebServer.takeRequest()
+
+        val path = request.path ?: ""
+        assertTrue(path.isNotEmpty())
+
+        assertEquals("/3/movie/${movieId}/reviews", path.substring(0, path.indexOf("?")))
+        assertTrue(path.contains(Constants.API_KEY))
+    }
+
+    @Test
+    fun `ensure the correct path is called when calling getMovieTrailers method in RestService`() = runTest {
+        val mockResponse = MockResponse()
+        mockWebServer.enqueue(mockResponse.setBody("{}"))
+
+        val movieId = 101
+        restService.getMovieTrailers(movieId)
+        val request = mockWebServer.takeRequest()
+
+        val path = request.path ?: ""
+        assertTrue(path.isNotEmpty())
+
+        assertEquals("/3/movie/${movieId}/videos", path.substring(0, path.indexOf("?")))
+        assertTrue(path.contains(Constants.API_KEY))
+    }
+
+    @After
+    fun teardown() {
+        mockWebServer.shutdown()
+    }
+}
